279292bef95973360ae38a8dc4855ca678f16a79
Athernatus <nizarmw1@gmail.com>
2024-11-17 18:54:16 +0800

    nambah page

---
+++ app/src/main/java/com/example/dailyexpense/LihatLaporan.kt
@@ -0,0 +1,20 @@
+package com.example.dailyexpense
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LihatLaporan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lihat_laporan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
